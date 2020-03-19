a012110e08129d9f3ca08b5a44ea873d8bb58aa8
Eduardo Lucero <eduardo.lucero@webit.com.au>
2020-03-19 15:26:54 +1100

    Completing back-end base

--- routes/product.js
+++ routes/product.js
@@ -1,17 +1,41 @@
 const express = require('express');
 const router = express.Router();
 
-const { create, productById, read, remove, update, list, listRelated } = require('../controllers/product');
-const { requireSignin, isAuth, isAdmin } = require('../controllers/auth')
-const { userById } = require('../controllers/user')
+const { 
+    create,
+    productById,
+    read,
+    remove,
+    update,
+    list,
+    listRelated,
+    listCategories,
+    listBySearch,
+    photo
+} = require('../controllers/product');
 
+const {
+    requireSignin,
+    isAuth,
+    isAdmin
+} = require('../controllers/auth')
+
+const { 
+    userById
+} = require('../controllers/user')
+
+// Product Managment
 router.get('/product/:productId', read, remove);
 router.post('/product/create/:userId', requireSignin, isAdmin, isAuth, create);
 router.delete('/product/:productId/:userId', requireSignin, isAuth, isAdmin, remove);
 router.put('/product/:productId/:userId', requireSignin, isAuth, isAdmin, update);
 
+// Returning Products
 router.get('/products', list);
 router.get('/products/related/:productId', listRelated);
+router.get('/products/categories', listCategories);
+router.post('/products/by/search', listBySearch);
+router.get('product/photo/:productId', photo);
 
 router.param('userId', userById);
 router.param('productId', productById);
--- routes/user.js
+++ routes/user.js
@@ -2,14 +2,17 @@ const express = require('express');
 const router = express.Router();
 
 const { requireSignin, isAuth, isAdmin } = require('../controllers/auth');
-const { userById } = require('../controllers/user');
+const { userById, read, update } = require('../controllers/user');
 
 router.get('/secret/:userId', requireSignin, isAuth, isAdmin, (req, res) => {
     res.json({
         user: req.profile
-    })
-})
+    });
+});
 
-router.param('userId', userById)
+router.get('/user/:userId', requireSignIn, isAuth, read);
+router.put('./user/:userId', requireSignIn, isAuth, update);
+
+router.param('userId', userById);
 
 module.exports = router;
